601395247e2c27fecf48f44dba842298722c83c4
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-06-12 11:34:59 +0900

    Add modal for changing student

--- components/StudentBorder.tsx
+++ components/StudentBorder.tsx
@@ -1,10 +1,11 @@
 import { useStudent } from "@/context/StudentContext"
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import { styled } from "styled-components/native";
 import { StudentAvatar } from "./StudentAvatar";
 import { PressableAvatarPane, } from "./ThemedView";
 import { SemiboldLightText, TitleText } from "./ThemedText";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { ChangeStudentDialog, StudentList } from "./StudentList";
 
 type BorderProps = {
   $tabHeight: number;
@@ -32,9 +33,18 @@ const HeaderTextFrame = styled.View`
 
 `
 
+const AvatarFrame = styled.View`
+  position : relative;
+`
+
 export const StudentBorder = ({ children, title, subtitle }: StudentBorderProps) => {
   const { student } = useStudent();
+  const [showList, setShowList] = useState(false);
   const tabBarHeight = useBottomTabBarHeight();
+
+  const toggleList = () => {
+    showList ? setShowList(false) : setShowList(true);
+  }
 
   return (
     <BorderView $tabHeight={tabBarHeight}>
@@ -44,11 +54,12 @@ export const StudentBorder = ({ children, title, subtitle }: StudentBorderProps)
             <TitleText>{title}</TitleText>
             <SemiboldLightText>{subtitle}</SemiboldLightText>
           </HeaderTextFrame>
-          <PressableAvatarPane onPress={() => console.log("Change Student")}>
+          <PressableAvatarPane onPress={toggleList}>
             <StudentAvatar url={student?.imagesrc} width={48} height={48} style="full" />
           </PressableAvatarPane>
         </HeaderFrame>
       }
+      {showList && (<ChangeStudentDialog onDismiss={() => setShowList(false)} />)}
       {children}
     </BorderView>
   )
