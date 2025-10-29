6f27df717b7cae274de7da2330c1e56b88a6bb7a
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-29 12:51:11 +0900

    Define types for feed responses

---
+++ constants/studentFeedEmotions.ts
@@ -0,0 +1,29 @@
+export const StudentFeedEmotions = {
+  HAPPY: {
+    emoji: "&#x1F60A;"
+  },
+  SAVORING: {
+    emoji: "&#x1F60B;"
+  },
+  SLEEPY: {
+    emoji: "&#x1F634;"
+  },
+  ANGRY: {
+    emoji: "&#x1F621;"
+  },
+  SAD: {
+    emoji: "&#x1F62D;"
+  },
+  THOUGHTFUL: {
+    emoji: "&#x1F914;"
+  },
+  SICK: {
+    emoji: "&#x1F912;"
+  },
+  TIRED: {
+    emoji: "&#x1F971;"
+  },
+  QUIET: {
+    emoji: "&#x1F910;"
+  }
+} as const;
