734aaef206fe755a394774bd8d39036f3a958f60
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-11-24 16:40:07 +0900

    Change API address

--- api/api.ts
+++ api/api.ts
@@ -1,13 +1,5 @@
-import { Platform } from "react-native";
-
 const getApiBaseUrl = () => {
-  if (Platform.OS === "ios") {
-    return process.env.EXPO_PUBLIC_IOS_API_URL
-  } else if (Platform.OS === "android") {
-    return process.env.EXPO_PUBLIC_ANDROID_API_URL
-  } else {
-    return process.env.EXPO_PUBLIC_PHYSICAL_API_URL
-  }
+  return "https://deulaubaba-server-426021917364.asia-northeast3.run.app"
 };
 
 export const API_BASE_URL = getApiBaseUrl();
